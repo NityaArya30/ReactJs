09536dd1b600e7183db05c0b840f459052b07d9e
Nitya Arya <nityaarya11@gmail.com>
2024-05-12 15:57:32 +0530

    files added

--- 12BlogAppwrite/src/main.jsx
+++ 12BlogAppwrite/src/main.jsx
@@ -4,11 +4,33 @@ import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import store from './store/store.js'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { AuthLayout } from './components/index.js'
+const router =createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <Home />
+      },
+      {
+        path: '/login',
+        element: (
+          <AuthLayout authentication={false}>
+            <Login />
+          </AuthLayout>
+        )
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
   <Provider store={store}>
-  <App />
+  <RouterProvider router = {router}/>
   </Provider>
   </React.StrictMode>,
 )
